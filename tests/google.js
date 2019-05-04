165e7188361f0e29e77038307b764c23b25822fe
Aditya Inapurapu <adityaii@gmail.com>
2019-05-04 16:38:02 -0700

    Initial commit

---
+++ tests/google.js
@@ -0,0 +1,14 @@
+module.exports = {
+   tags: ['google'],
+   'Demo test Google' : function (browser) {
+        browser 
+        .url('http://www.google.com') // Go to a url
+        .waitForElementVisible('body', 1000) // wait till page loads
+        .assert.title('Google') // Make sure Site title matches
+        .assert.visible('[name="q"]') 
+        .setValue('[name="q"]', 'nightwatchjs') // send values 
+        .click('div.FPdoLc.VlcLAe > center > input:nth-of-type(1)') // click on search box
+        .pause(1000)
+        .end();
+      }
+};
