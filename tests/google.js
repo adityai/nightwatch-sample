c7c97e9bfe6f3ef274236f0d8a1b7ffba4f0f497
Aditya Inapurapu <adityaii@gmail.com>
2019-09-29 18:49:18 -0700

    Adding google iaditya.com search test

--- tests/google.js
+++ tests/google.js
@@ -10,5 +10,17 @@ module.exports = {
         .click('div.FPdoLc.VlcLAe > center > input:nth-of-type(1)') // click on search box
         .pause(1000)
         .end();
-      }
+      },
+    'Test iaditya.com' : function (browser) {
+        browser
+            .url('http://www.google.com') // Go to a url
+            .waitForElementVisible('body', 1000) // wait till page loads
+            .assert.title('Google') // Make sure Site title matches
+            .assert.visible('[name="q"]')
+            .setValue('[name="q"]', 'iaditya.com') // send values
+            .click('div.FPdoLc.VlcLAe > center > input:nth-of-type(1)') // click on search box
+            .pause(1000)
+            .end();
+    }
+
 };
