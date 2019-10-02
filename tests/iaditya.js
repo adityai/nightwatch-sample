9b2df7f2801d36b087a761547f4800f2518c7acb
Aditya Inapurapu <adityaii@gmail.com>
2019-10-01 21:29:40 -0700

    Added iaditya.js

--- tests/iaditya.js
+++ tests/iaditya.js
@@ -4,5 +4,8 @@ module.exports = {
         browser
             .url('http://www.iaditya.com/')
             .assert.title('iAditya.com')
+            .click('#b-query')
+            .setValue('#b-query', 'iaditya.com')
+            .click('#b-query-icon')
     }
 };
